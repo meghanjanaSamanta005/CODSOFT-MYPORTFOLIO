a3297c10491371303f08a4422ea16d22f00bb5aa
meghnaSamanta <meghanjanasamantashau01@gmail.com>
2024-12-30 21:38:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Full Stack Developer", "Python Developer", "Graphic Designer","Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
